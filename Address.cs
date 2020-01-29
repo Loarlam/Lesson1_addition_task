c9eb4bc23da73fa041c5826cf032d29790d2067b
Loarlam <l9m_pam@live.ru>
2020-01-30 00:13:32 +0300

    Add project files.

---
+++ Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Addition_Task
+{
+    class Address
+    {
+        /*Поля можно не создавать, ибо свойства всё равно создают сокрытые поля*/
+        public int Index { get; set; }
+        public int House { get; set; }
+        public int Apartment { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Используя Visual Studio, создайте проект по шаблону Console Application.  
+Требуется:  
+Создать класс с именем Address. 
+В теле класса требуется создать поля: index, country, city, street, house, apartment.  Для каждого поля, создать свойство с двумя методами доступа.  
+Создать экземпляр класса Address.  
+В поля экземпляра записать информацию о почтовом адресе.  
+Выведите на экран значения полей, описывающих адрес. 
+*/
+
+using System;
+
+namespace Addition_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address();
+            address.Index = 125009;
+            address.House = 19;
+            address.Apartment = 9;
+            address.Country = "Russia";
+            address.City = "Moscow";
+            address.Street = "Tverskaya";
+            Console.WriteLine("My index is {0}.\nMy house is {1}.\nMy apartment number is {2}.\nMy country is {3}.\nMy city is {4}.\nMy street is {5}.", address.Index, address.House, address.Apartment, address.Country, address.City, address.Street);
+            Console.ReadKey();
+        }
+    }
+}
